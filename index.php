36a72a9cb87be46a584f93cd1c660d94805b863e
manolioluigi <luigi.manolio1999@gmail.com>
2023-01-30 16:08:40 +0100

    esercizio

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-12">
+                
+                <h1>Hotel disponibili:</h1>
+
+                <table class="table table-dark">
+                    <thead>
+                        <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col">Parcheggio</th>
+                        <th scope="col">Voto</th>
+                        <th scope="col">Distanza dal centro</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($hotels as $hotel){ ?>
+                               
+                            <tr>
+                                <td><?php echo $hotel['name'];?></td>
+                                <td><?php echo $hotel['description'];?></td>
+                                <td><?php 
+                                if($hotel['parking']){
+                                    echo 'Si';
+                                }else{
+                                    echo 'No';
+                                }
+                                ?></td>
+                                <td><?php echo $hotel['vote'];?></td>
+                                <td><?php echo $hotel['distance_to_center'];?></td>
+                            </tr>
+
+                        <?php } ?>
+                    </tbody>
+                </table>
+
+            </div>
+
+        </div>    
+
+    </div>
+
+</body>
+</html>
